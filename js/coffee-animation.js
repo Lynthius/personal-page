a43adac5210097992922f10f61a65e85ba65e543
Tomasz <t.s.przyborowski@gmail.com>
2020-04-13 18:27:47 +0200

    add coffee animation in webgl with pixi.js

---
+++ js/coffee-animation.js
@@ -0,0 +1,42 @@
+const contactSection = document.querySelector(".coffee-container");
+const coffeeCanvas = document.querySelector(".coffee-canvas");
+
+let coffeeApp;
+
+function initPixi() {
+    coffeeApp = new PIXI.Application({
+        width: 500,
+        height: 499,
+        view: coffeeCanvas
+    });
+
+    contactSection.appendChild(coffeeApp.view)
+
+    let coffeImg = new PIXI.Sprite.from("images/coffee-cup.png");
+    coffeImg.width = 500;
+    coffeImg.height = 499;
+
+    coffeeApp.stage.addChild(coffeImg);
+
+    displacementSprite = new PIXI.Sprite.from("images/clouds2.png");
+    displacementFilter = new PIXI.filters.DisplacementFilter(displacementSprite);
+
+    displacementSprite.texture.baseTexture.wrapMode = PIXI.WRAP_MODES.REPEAT;
+    coffeeApp.stage.addChild(displacementSprite);
+    coffeeApp.stage.filters = [displacementFilter];
+
+    coffeeApp.renderer.view.style.transform = "scale(1.02)";
+
+    displacementSprite.scale.x = 6;
+    displacementSprite.scale.y = 6;
+
+    animate();
+}
+
+function animate() {
+    displacementSprite.x += 7;
+    displacementSprite.y += 5;
+    requestAnimationFrame(animate);
+}
+
+initPixi();
