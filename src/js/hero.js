cd376e12d8eb304b220ae0a56d4d17f9e91347d8
Tomasz Przyborowski <t.s.przyborowski@gmail.com>
2020-08-20 20:23:16 +0200

    jpg/png to webp conversion

---
+++ src/js/hero.js
@@ -0,0 +1,31 @@
+const heroSection = document.querySelector(".hero-section");
+const heroCanvas = document.querySelector(".hero-canvas");
+
+// Hero canvas animation
+let heroApp = new PIXI.Application({
+    width: 945,
+    height: 900,
+    view: heroCanvas
+});
+
+heroApp.renderer.view.style.zIndex = "-1";
+heroApp.renderer.backgroundColor = 0xFFFFFF;
+
+heroSection.appendChild(heroApp.view);
+
+let heroImg = new PIXI.Sprite.from("assets/images/hero-image.webp");
+heroImg.width = 945;
+heroImg.height = 900;
+
+heroApp.stage.addChild(heroImg);
+
+depthHeroMap = new PIXI.Sprite.from("assets/images/hero-image-map.webp");
+heroApp.stage.addChild(depthHeroMap);
+
+displacementHeroFilter = new PIXI.filters.DisplacementFilter(depthHeroMap);
+heroApp.stage.filters = [displacementHeroFilter];
+
+window.onmousemove = function (e) {
+    displacementHeroFilter.scale.x = (window.innerWidth / 2 - e.clientX) / 65;
+    displacementHeroFilter.scale.y = (window.innerHeight / 2 - e.clientY) / 45;
+};
