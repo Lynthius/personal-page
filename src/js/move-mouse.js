205f8fea8e12770b36337a57d976b035eb623a95
lynthius <t.s.przyborowski@gmail.com>
2020-11-22 19:17:02 +0100

    fix hideMouseInfo function

--- src/js/move-mouse.js
+++ src/js/move-mouse.js
@@ -2,22 +2,37 @@ const circle = document.querySelector(".circle");
 const anchors = document.querySelectorAll("a");
 const btns = document.querySelectorAll("button");
 
-anchors.forEach((anchor) => {
-  anchor.onmouseover = function () {
+function hideCursorInfo() {
+  if (percentage > 20) {
     circle.style.opacity = 0;
-  };
-  anchor.onmouseleave = function () {
+  } else if (percentage < 20) {
     circle.style.opacity = 1;
-  };
+  }
+}
+window.addEventListener("scroll", hideCursorInfo);
+
+anchors.forEach((anchor) => {
+  if (percentage < 20) {
+    anchor.onmouseover = function () {
+      circle.style.opacity = 0;
+    };
+    anchor.onmouseleave = function () {
+      if (percentage < 20) {
+        circle.style.opacity = 1;
+      }
+    };
+  }
 });
 
 btns.forEach((btn) => {
   btn.onmouseover = function () {
     circle.style.opacity = 0;
   };
-  btn.onmouseleave = function () {
-    circle.style.opacity = 1;
-  };
+  if (percentage < 20) {
+    btn.onmouseleave = function () {
+      circle.style.opacity = 1;
+    };
+  }
 });
 
 document.addEventListener("mousemove", function (e) {
@@ -37,12 +52,3 @@ window.onload = function () {
     }, 1000000000);
   }, 500);
 };
-
-function hideLangSwitch() {
-  if (percentage > 20) {
-    circle.style.opacity = 0;
-  } else if (percentage < 20) {
-    circle.style.opacity = 1;
-  }
-}
-window.addEventListener("scroll", hideLangSwitch);
