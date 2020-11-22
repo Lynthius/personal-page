585140693480eb5e9403d028cd8ee67fc8d67714
lynthius <t.s.przyborowski@gmail.com>
2020-11-22 18:41:17 +0100

    add hide mouse info on hover

--- src/js/move-mouse.js
+++ src/js/move-mouse.js
@@ -1,26 +1,48 @@
-const cursor = document.querySelector(".cursor");
+const circle = document.querySelector(".circle");
+const anchors = document.querySelectorAll("a");
+const btns = document.querySelectorAll("button");
+
+anchors.forEach((anchor) => {
+  anchor.onmouseover = function () {
+    circle.style.opacity = 0;
+  };
+  anchor.onmouseleave = function () {
+    circle.style.opacity = 1;
+  };
+});
+
+btns.forEach((btn) => {
+  btn.onmouseover = function () {
+    circle.style.opacity = 0;
+  };
+  btn.onmouseleave = function () {
+    circle.style.opacity = 1;
+  };
+});
 
 document.addEventListener("mousemove", function (e) {
   let x = e.clientX;
   let y = e.clientY;
-  cursor.style.left = x + 30 + "px";
-  cursor.style.top = y + 30 + "px";
+  circle.style.left = x - 105 + "px";
+  circle.style.top = y - 105 + "px";
 });
 
 let timeout;
 window.onload = function () {
   clearTimeout(timeout);
   timeout = setTimeout(function () {
-    cursor.style.opacity = 1;
+    circle.style.opacity = 1;
     setTimeout(function () {
-      cursor.style.opacity = 0;
-    }, 10000);
+      circle.style.opacity = 0;
+    }, 1000000000);
   }, 500);
 };
 
 function hideLangSwitch() {
   if (percentage > 20) {
-    cursor.style.opacity = 0;
+    circle.style.opacity = 0;
+  } else if (percentage < 20) {
+    circle.style.opacity = 1;
   }
 }
 window.addEventListener("scroll", hideLangSwitch);
